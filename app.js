aab3439e507efce76091e946fa80882168cf5357
Jaffet913 <jaffet.tubens@upr.edu>
2021-12-01 21:34:49 -0400

    Backend

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express =  require('express')
 const users = require('./data').userDB
 const path = require('path')
+const bcrypt = require('bcrypt')
 
 const app = express()
 const port = 3000
+const userprofile =[]
 
 //load Static files
 app.use(express.static('public'))
@@ -25,6 +27,8 @@ const loginRouter = require('./src/routes/login')
 app.use('', indexRouter)
 app.use('/login', loginRouter)
 app.use('/css', express.static(path.join('/node_modules/bootstrap/dist/css')))
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
 
 
 //home routing
@@ -33,12 +37,62 @@ app.get('', (req, res) => {
     res.render('index')
 })
 
-app.get('/login', (req, res) => {
+app.get('/login', async (req, res) => {
+    const usermain = userprofile.find(usermain => userprofile.name = req.body.name)
+    if(usermain == null){
+        return res.status(400).send("The current information is not recorded")
+    }
+
+    try{
+       if(await bcrypt.compare(req.body.password, usermain.password)){
+        res.redirect('collections')
+       }else{
+        res.send("Information is Incorrect")
+       }
+    }catch{
+        res.status(500).send()
+    }
     res.render('login')
+    res.json(usermain)
+})
+
+// app.post('/users', async(req, res) => {
+//     try{
+//         const salt = bcrypt.genSalt()
+//         const passwordHashed = await bcrypt.hash(req.body.password, salt)
+//         console.log(passwordHashed)
+//         console.log(salt)
+//         const user ={username: req.body.username, password: passwordHashed} 
+//         users.push(user)
+//         res.status(201).send()
+//     }catch{
+//         res.status(500).send
+//     }
+
+// })
+
+app.post('/login', (req, res) => {
+
 })
 
 app.get('/register', (req, res) => {
     res.render('register')
+})
+
+app.post('/register', async(req, res) => {
+    try{
+        const salt = bcrypt.genSalt()
+        const passwordHashed = await bcrypt.hash(req.body.password, salt)
+        console.log(passwordHashed)
+        console.log(salt)
+        const user ={email: req.body.email, username: req.body.username, password: passwordHashed} 
+        userprofile.push(user)
+        res.status(201).send()
+        res.redirect('login')
+    }catch{
+        res.redirect('register')
+    }
+    console.log(userprofile)
 })
 
 app.get('/profile', (req, res) => {
